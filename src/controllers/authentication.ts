0f4e75d2cc8ec942114223f93787282b4ab58167
Eric Yun <yunseric@gmail.com>
2024-04-05 17:25:38 -0700

    Add createUser function and update register endpoint

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,5 +1,7 @@
 import express from 'express'
-import { getUserByEmail } from '../db/users'
+
+import { createUser, getUserByEmail } from '../db/users'
+import { authentication, random } from '../helpers'
 
 export const register = async(req: express.Request, res: express.Response) => {
   try{
@@ -13,6 +15,19 @@ export const register = async(req: express.Request, res: express.Response) => {
     if(existingUser) {
       return res.sendStatus(400)
     }
+
+    const salt = random()
+
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password)
+      }
+    })
+
+    return res.status(200).json(user).end()
 
   } catch(error) {
     console.log(error);
