8e8b0c969321bb4d839d7f3b145e8b4f67d65fe3
Eric Yun <yunseric@gmail.com>
2024-04-12 17:14:52 -0700

    Add lodash dependency and update login functionality

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -11,19 +11,26 @@ export const login = async(req: express.Request, res: express.Response) => {
       return res.sendStatus(400)
     }
 
-    const user = await getUserByEmail(email)
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
 
     if (!user) {
       return res.sendStatus(400)
     }
-
-
 
     const expectedHash = authentication(user.authentication?.salt ?? '', password)
 
     if(user.authentication?.password !== expectedHash) {
       return res.sendStatus(403)
     }
+
+    const salt = random()
+    user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+    await user.save()
+
+    res.cookie('ERIC-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/'})
+
+    return res.status(200).json(user).end()
 
 
   } catch (error){
