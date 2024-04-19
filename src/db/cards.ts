b7dab6c440c178b263cb98d732bd9cfb6382a683
Eric Yun <yunseric@gmail.com>
2024-04-18 21:28:06 -0700

    Update OnePieceCard schema in cards.ts

--- src/db/cards.ts
+++ src/db/cards.ts
@@ -1,8 +1,30 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-const CardSchema = new mongoose.Schema({
+export interface OnePieceCard extends Document {
+  name: string;
+  type: string;
+  rarity: string;
+  description: string;
+  imageUrl: string;
+  triggerEffect?: string
+  // Add more fields as needed
+}
+
+/*
+  ONE PIECE TCG card schema
+
+
+*/
+
+const CardSchema: Schema = new Schema({
   name: { type: String, required: true },
-  type: { type: String, required: true },
-  color: { type: String, required: true },
-  set: { type: String, required: true },
+  type: { type: String, required: true }, // 
+  color: { type: String[], required: true }, // color inclu
+  set: { type: String, required: true }, // gona be set box the card is from (e.g. "OP-01")
+  imageUrl: { type: String, required: false},
+  cardId: {type: String, required: true},
+  triggerEffect: { type: String }
+
 })
+
+export default mongoose.model<OnePieceCard>('OnePieceCard', CardSchema)
